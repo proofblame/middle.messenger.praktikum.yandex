14c83b95574f3c91e0a185648b7ab185bb221a8b
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2023-01-13 20:10:30 +0300

    server and readme

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + "/dist"));
+
+const navLinks = [
+  "/",
+  "/signin",
+  "/signup",
+  "/account",
+  "/reset-password",
+  "/500",
+  "/400",
+];
+
+navLinks.forEach(() => {
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/dist/index.html");
+  });
+});
+
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}!`);
+});
--- src/index.js
+++ src/index.js
@@ -6,9 +6,34 @@ import Chat from "./pages/chat";
 import Error from "./pages/error";
 import UpdatePass from "./pages/update-pass";
 
+const path = window.location.pathname;
 const root = document.getElementById("root");
-root.innerHTML = UpdatePass();
-// root.innerHTML = Error({
-//   number: 500,
-//   message: "Мы уже фиксим",
-// });
+
+switch (path) {
+  case "/":
+    root.innerHTML = Chat();
+    break;
+  case "/signin":
+    root.innerHTML = Login();
+    break;
+  case "/signup":
+    root.innerHTML = Register();
+    break;
+  case "/account":
+    root.innerHTML = Profile();
+    break;
+  case "/reset-password":
+    root.innerHTML = UpdatePass();
+    break;
+  case "/500":
+    root.innerHTML = Error({
+      number: 500,
+      message: "Мы уже фиксим",
+    });
+    break;
+  default:
+    root.innerHTML = Error({
+      number: 404,
+      message: "Не туда попали",
+    });
+}
