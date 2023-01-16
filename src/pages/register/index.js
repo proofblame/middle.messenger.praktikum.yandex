93fe4011651a8bc66b09df2ec549806e0d8bf61a
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2023-01-16 18:11:08 +0300

    fix

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -4,13 +4,13 @@ import Form from "../../components/form";
 import Container from "../../components/container";
 
 const inputs = [
-  Input({ id: "email", name: "Почта", type: "text" }),
-  Input({ id: "login", name: "Логин", type: "text" }),
-  Input({ id: "first_name", name: "Имя", type: "text" }),
-  Input({ id: "second_name", name: "Фамилия", type: "text" }),
-  Input({ id: "phone", name: "Телефон", type: "tel" }),
-  Input({ id: "password", name: "Пароль", type: "password" }),
-  Input({ id: "confirm-password", name: "Пароль (еще раз)", type: "password" }),
+  Input({ name: "email", title: "Почта", type: "text"}),
+  Input({ name: "login", title: "Логин", type: "text"}),
+  Input({ name: "first_name", title: "Имя", type: "text"}),
+  Input({ name: "second_name", title: "Фамилия", type: "text"}),
+  Input({ name: "phone", title: "Телефон", type: "tel"}),
+  Input({ name: "password", title: "Пароль", type: "password"}),
+  Input({ name: "confirm_password", title: "Пароль (еще раз)", type: "password"}),
 ];
 
 const form = Form({
