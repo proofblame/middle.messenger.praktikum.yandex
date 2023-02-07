7e81c3fb08fe203686ec9cb1bc7ab3c1d1e5ddce
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2023-02-07 19:31:02 +0300

    fix

--- src/components/info-row/info-row.tpl.ts
+++ src/components/info-row/info-row.tpl.ts
@@ -1,17 +1,17 @@
 export const tpl = `
-<div class="info__row">
+<div class="info-row">
   {{#if info}}
-  <div class="info__row-wrapper">
-    <label for="{{id}}" class="info__row-label">{{item}}
+  <div class="info-row-wrapper">
+    <label for="{{id}}" class="info-row-label">{{item}}
     </label>
-    <input id="{{id}}" name="{{id}}" {{disabled}} class="info__row-input" value="{{info}}" {{#if required}}
+    <input id="{{id}}" name="{{id}}" {{disabled}} class="info-row-input" value="{{info}}" {{#if required}}
       data-required="{{required}}"
       {{/if}}
       type="{{type}}" />
-    <span class="info__row-error"></span>
+    <span class="info-row-error"></span>
   </div>
   {{else}}
-  <button class="info__row-button {{className}}">{{item}}</button>
+  <button class="info-row-button {{className}}">{{item}}</button>
   {{/if}}
 </div>
 `;
